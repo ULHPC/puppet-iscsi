1ef48b0a887085cb1c88c73f192a616efa8e9087
Sarah Diehl <sarah.diehl@uni.lu>
2015-09-28 14:24:59 +0200

    update content of 'params.pp'
    Signed-off-by: Sarah Diehl <sarah.diehl@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -15,12 +15,20 @@
 
 include 'iscsi::params'
 
-$names = ['ensure', 'protocol', 'port', 'packagename']
+$names = ["ensure", "protocol", "port", "packagename", "servicename", "configfile", "configfile_init", "debian_init", "debian_init_mode", "configfile_mode", "configfile_owner", "configfile_group"]
 
 notice("iscsi::params::ensure = ${iscsi::params::ensure}")
 notice("iscsi::params::protocol = ${iscsi::params::protocol}")
 notice("iscsi::params::port = ${iscsi::params::port}")
 notice("iscsi::params::packagename = ${iscsi::params::packagename}")
+notice("iscsi::params::servicename = ${iscsi::params::servicename}")
+notice("iscsi::params::configfile = ${iscsi::params::configfile}")
+notice("iscsi::params::configfile_init = ${iscsi::params::configfile_init}")
+notice("iscsi::params::debian_init = ${iscsi::params::debian_init}")
+notice("iscsi::params::debian_init_mode = ${iscsi::params::debian_init_mode}")
+notice("iscsi::params::configfile_mode = ${iscsi::params::configfile_mode}")
+notice("iscsi::params::configfile_owner = ${iscsi::params::configfile_owner}")
+notice("iscsi::params::configfile_group = ${iscsi::params::configfile_group}")
 
 #each($names) |$v| {
 #    $var = "iscsi::params::${v}"
